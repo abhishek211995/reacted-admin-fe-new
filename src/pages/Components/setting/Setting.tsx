fc0172dddd94323d8a0e794b7dbdf452b8677964
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-03-30 18:08:14 +0530

    initial commit

---
+++ src/pages/Components/setting/Setting.tsx
@@ -0,0 +1,30 @@
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+import { SettingsPassword } from './SettingPassword'
+
+const Setting = () => {
+    return (
+        <Box
+            component="main"
+            sx={{
+                flexGrow: 1,
+                py: 8
+            }}
+        >
+            <Container maxWidth="lg">
+                <Typography
+                    sx={{ mb: 3 }}
+                    variant="h4"
+                >
+                    Settings
+                </Typography>
+                {/* <SettingsNotifications />  */}
+                <Box sx={{ pt: 3 }}>
+                    <SettingsPassword />
+                </Box>
+            </Container>
+        </Box>
+    )
+}
+
+export default Setting
